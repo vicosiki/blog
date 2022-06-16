51a1723af4cae528a1466fc813a022248979283b
vicosiki <victor.osiki@gmail.com>
2022-06-16 15:27:51 +0100

    Creates a new model for the blog app

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+# Create your models here.
+class Post(models.Model): 
+
+    Title = models.CharField(max_length = 200) 
+    Text = models.TextField() 
+    Created_date = models.DateTimeField() 
+    Published_date = models.DateTimeField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
